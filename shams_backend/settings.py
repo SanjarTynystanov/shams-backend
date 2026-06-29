36aa8048bc9e11d4cfb4e867e0e1bf9d6db77413
Sanjar <tynystanov.s.u@example.com>
2026-06-29 17:42:40 +0600

    fix: settings and static files

--- shams_backend/settings.py
+++ shams_backend/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-your-secret-key-here-change-this-in-production'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True').lower() != 'false'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
